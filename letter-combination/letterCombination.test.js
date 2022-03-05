f7b3e17e700e042a794faf397ac2e4c39065dd3c
Fernando Barrios - jfergt <fer.b.1990@gmail.com>
2022-03-05 11:03:50 -0600

    feat: Add 3sum test (#1)
    * Update package.json
    
    * Create 3sum.test.js
    
    * Update letterCombination.test.js

--- letter-combination/letterCombination.test.js
+++ letter-combination/letterCombination.test.js
@@ -1,21 +1,41 @@
 import letterCombination from "./letterCombination";
 
 test("letterCombination: 2 digits", () => {
-  expect(letterCombination("23")).toStrictEqual([
-    "ad",
-    "ae",
-    "af",
-    "bd",
-    "be",
-    "bf",
-    "cd",
-    "ce",
-    "cf",
-  ]);
+  expect(
+    letterCombination("23").sort((a, b) => a.localeCompare(b))
+  ).toStrictEqual(["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]);
 });
 
 test("letterCombination: 3 digits", () => {
-    expect(letterCombination("234")).toStrictEqual([
-      "adg", "adh", "adi", "aeg", "aeh", "aei", "afg", "afh", "afi", "bdg", "bdh", "bdi", "beg", "beh", "bei", "bfg", "bfh", "bfi", "cdg", "cdh", "cdi", "ceg", "ceh", "cei", "cfg", "cfh", "cfi",
-    ]);
-  });
+  expect(
+    letterCombination("234").sort((a, b) => a.localeCompare(b))
+  ).toStrictEqual([
+    "adg",
+    "adh",
+    "adi",
+    "aeg",
+    "aeh",
+    "aei",
+    "afg",
+    "afh",
+    "afi",
+    "bdg",
+    "bdh",
+    "bdi",
+    "beg",
+    "beh",
+    "bei",
+    "bfg",
+    "bfh",
+    "bfi",
+    "cdg",
+    "cdh",
+    "cdi",
+    "ceg",
+    "ceh",
+    "cei",
+    "cfg",
+    "cfh",
+    "cfi",
+  ]);
+});
