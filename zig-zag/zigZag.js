ae16bfcaa1c8092b0965dcf13876eb3151f5e174
Fernando Barrios <fer.b.1990@gmail.com>
2022-03-27 19:45:41 -0600

    fix: cambiar asc por desc

--- zig-zag/zigZag.js
+++ zig-zag/zigZag.js
@@ -6,19 +6,19 @@ var convert = function (s, numRows) {
   const matrix = [...new Array(numRows)].map(() => []);
 
   let r = 0;
-  let ord = "asc";
+  let ord = "desc";
 
   for (let i = 0; i < s.length; i++) {
     const c = s.charAt(i);
     matrix[r].push(c);
 
-    if (ord === "asc") {
+    if (ord === "desc") {
       r++;
 
-      if (r === numRows - 1) ord = "desc";
+      if (r === numRows - 1) ord = "asc";
     } else {
       r--;
-      if (r === 0) ord = "asc";
+      if (r === 0) ord = "desc";
     }
   }
 
